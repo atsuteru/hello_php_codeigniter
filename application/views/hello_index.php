a59e0be05364da4e3a1580b1b73af51e3e2949fd
OKAI.Mitsuo <okai@kk-brain.jp>
2016-01-21 19:18:39 +0900

    初期登録

---
+++ application/views/hello_index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>
+        <?php
+        echo $message;
+        ?>
+        </p>
+        <p>
+            <input type="text" value="<?=$message?>"/> 
+        </p>
+    </body>
+</html>
